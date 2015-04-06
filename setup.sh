0d08c3fed34514cc5ccf46cdfe8924fa2f599816
Taesung Jung <tsj.july@gmail.com>
2015-04-06 00:35:14 -0700

    added a command * remove directory ~/.vim

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ cp -v .bash_profile ~/.bash_profile
 cp -v .bashrc ~/.bashrc
 
 # setup vim
+rm -rf ~/.vim/bundle/
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp -v .vimrc ~/.vimrc
 vim +PluginInstall +qall

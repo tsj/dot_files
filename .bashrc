950e81912c7ab613b21493db343a8e3e0e83f4c8
Taesung Jung <tsj.july@gmail.com>
2015-04-14 18:09:32 -0700

    fixed bugs

--- .bashrc
+++ .bashrc
@@ -1,9 +1,11 @@
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
 export PS1="\[\e[1;31m\]\H:\u@pts/\l \[\e[0m\]\[\e[1;32m\]\d \t\[\e[0m\]\n\[\e[1;34m\]\w\e[1;32m\]\$(__git_ps1)\e[0m\] \$ "
-alias l='ls -GFh --color=auto'
-alias ls='ls -GFh --color=auto'
-alias ll='ls -FGlAhp --color=auto'
+alias l='ls -GFh'
+alias ls='ls -GFh'
+alias ll='ls -FGlAhp'
 
 export HISTFILESIZE=
 export HISTSIZE=
+
+export GOPATH="$HOME/go"
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 # download dot files
-echo -e "\033[34mDownloading dot files\033[0m"
+echo "\033[34mDownloading dot files\033[0m"
 curl https://raw.githubusercontent.com/tsj/dot_files/master/.bashrc -o ~/.bashrc
 curl https://raw.githubusercontent.com/tsj/dot_files/master/.bash_profile -o ~/.bash_profile
 curl https://raw.githubusercontent.com/tsj/dot_files/master/.vimrc -o ~/.vimrc
@@ -9,14 +9,14 @@ curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-com
 
 # download Vundle.vim
 rm -rf ~/.vim/bundle/
-echo -e "\033[34mDownloading Vundle.vim\033[0m"
+echo "\033[34mDownloading Vundle.vim\033[0m"
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Installing vim plugins
-echo -e "\033[34mInstalling vundle plugin\033[0m"
+echo "\033[34mInstalling vundle plugin\033[0m"
 vim +PluginInstall +qall
-echo -e "\033[34mInstalling go binaries\033[0m"
+echo "\033[34mInstalling go binaries\033[0m"
 vim +GoInstallBinaries +qall
-echo -e "\033[32mDone\033[0m"
 
 source ~/.bash_profile
+echo "\033[32mDone\033[0m"
